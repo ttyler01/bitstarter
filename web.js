24a91b47aa0c5d5112de8e8fb02ccc93346b67bb
Tim Tyler <ttyler01@gmail.com>
2013-07-09 15:30:35 +0000

    Fixed typo

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldi!');
+  response.send('Hello Worldi 2');
 });
 
 var port = process.env.PORT || 5000;
